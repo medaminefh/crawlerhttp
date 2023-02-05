df1e9017b576349144e0e8d73f8b4690f119ff7f
Mohamed Amine Fh <fhal.mohamed.amine@gmail.com>
2023-02-05 09:30:05 +0100

    add some tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,11 @@
 function normalizeUrl(url) {
-  return url;
+  const urlObj = new URL(url);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath && hostPath.endsWith("/")) {
+    return hostPath.slice(0, -1);
+  }
+
+  return hostPath;
 }
 
 module.exports = normalizeUrl;
--- crawl.test.js
+++ crawl.test.js
@@ -2,8 +2,29 @@ const normalizeUrl = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeUrl", () => {
-  const input = "";
+  const input = "https://www.google.com/path";
   const output = normalizeUrl(input);
-  const expected = "";
+  const expected = "www.google.com/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl with slashes /", () => {
+  const input = "https://www.google.com/path/";
+  const output = normalizeUrl(input);
+  const expected = "www.google.com/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl with Capitals", () => {
+  const input = "https://www.GOOGLE.com/path";
+  const output = normalizeUrl(input);
+  const expected = "www.google.com/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl with http", () => {
+  const input = "http://www.GOOGLE.com/path";
+  const output = normalizeUrl(input);
+  const expected = "www.google.com/path";
   expect(output).toEqual(expected);
 });
